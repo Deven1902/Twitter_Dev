5f29da400f9b1d5f7b5a6cbd37798498cb7ac40b
Deven1902 <devenamazingnandapurkar@gmail.com>
2023-11-25 09:33:18 +0530

    Added image upload in tweet api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -12,7 +12,7 @@ router.post('/tweets', createTweet);
 router.get('/tweets/:id', getTweet);
 router.post('/likes/toggle',toggleLike);
 // router.post('/comments', createComment);
-router.post('/comments', authenticate, createComment);
+router.post('/comments', createComment);
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/tweets', authenticate, createTweet);
